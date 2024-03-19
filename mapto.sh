099f2fdf5d93795d73131b100e4aec72d6ab4aef
Taavi Päll <tapa741@gmail.com>
2024-03-19 11:07:30 +0200

    Update mapto.sh

--- mapto.sh
+++ mapto.sh
@@ -3,5 +3,5 @@
 jid=`sbatch bt2-index.sh | awk '{print $NF }'`
 echo $jid
 
-jid=`sbatch --dependency=afterok:$jid bt2-map.sh | awk '{print $NF }'`
+jid=`sbatch --dependency=afterok:$jid --kill-on-invalid-dep=yes bt2-map.sh | awk '{print $NF }'`
 echo $jid
